bba1a79f873bcd98d12e9b0c29970f43ddbc8ce2
Evgeny Kuptsov <kupzov200@mail.ru>
2026-01-06 02:02:15 +0400

    feat: add resolve for formatDate and getWorkShedule task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -153,8 +153,24 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const fullDate = new Date(date);
+  const dateCurr = `${fullDate.getUTCMonth() + 1}/${fullDate.getUTCDate()}/${fullDate.getUTCFullYear()}`;
+
+  const hoursUTC = fullDate.getUTCHours();
+  const hours = hoursUTC > 12 ? hoursUTC - 12 : hoursUTC;
+  const minutesUTC = fullDate.getUTCMinutes();
+  const secondsUTC = fullDate.getUTCSeconds();
+
+  let AMPM = 'AM';
+  if (
+    hoursUTC > 12 ||
+    (hoursUTC === 12 && minutesUTC === 59 && secondsUTC === 59)
+  ) {
+    AMPM = 'PM';
+  }
+  const timeCurr = `${hours}:${String(fullDate.getUTCMinutes()).padStart(2, '0')}:${String(fullDate.getUTCSeconds()).padStart(2, '0')} ${AMPM}`;
+  return `${dateCurr}, ${timeCurr}`;
 }
 
 /**
@@ -259,8 +275,32 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const start = period.start.split('-');
+  const startDate = new Date(start[2], start[1] - 1, start[0]);
+
+  const end = period.end.split('-');
+  const endDate = new Date(end[2], end[1] - 1, end[0]);
+
+  const result = [];
+  let countDay = 0;
+
+  while (startDate <= endDate) {
+    const day = startDate.getDate().toString().padStart(2, '0');
+    const month = (startDate.getMonth() + 1).toString().padStart(2, '0');
+    const year = startDate.getFullYear();
+
+    if (countDay < countWorkDays) {
+      result.push(`${day}-${month}-${year}`);
+      startDate.setDate(startDate.getDate() + 1);
+      countDay += 1;
+    } else {
+      countDay = 0;
+      startDate.setDate(startDate.getDate() + countOffDays);
+    }
+  }
+
+  return result;
 }
 
 /**
