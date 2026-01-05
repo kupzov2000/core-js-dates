cfde4a40404b17ec200ad559799f33f9de417ac6
Evgeny Kuptsov <kupzov200@mail.ru>
2026-01-05 23:02:44 +0400

    feat: add resolve for getCountWeekendsInMonth task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -169,8 +169,16 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const totalDayMonth = new Date(year, month, 0).getDate();
+  let count = 0;
+  for (let i = 1; i <= totalDayMonth; i += 1) {
+    const currDay = new Date(year, month - 1, i).getDay();
+    if (currDay === 0 || currDay === 6) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 /**
